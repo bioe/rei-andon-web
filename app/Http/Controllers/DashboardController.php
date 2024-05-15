d2f328d155d98e2fa88d7b2a80dd5576b1ffa6c8
Eric <erickk89@gmail.com>
2024-05-15 18:17:57 +0800

    dashboard show response user data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Machine;
 use App\Models\Watch;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -31,10 +32,21 @@ class DashboardController extends Controller
 
     public function refreshMachine()
     {
-        $machines = Machine::with('machine_type')->with('last_status_record.status')->with('last_status_record.attended')->where('active', true)->get();
+        $time = Carbon::now()->subMinute(LATEST_RECORD_VIEW_MINUTE);
+
+        $machines = Machine::with('machine_type')
+            ->with('last_status_record.status')->with('last_status_record.attended', function ($query) use ($time) {
+                $query->where('created_at', '>=', $time);
+            })
+            ->where('active', true)->get();
 
         $group_machines = [];
+
         foreach ($machines as $m) {
+            if ($m->last_status_record != null && $m->last_status_record->created_at->lt($time)) {
+                $m->last_status_record = null;
+                $m->setRelation('last_status_record', null);
+            }
             $group_machines[$m->machine_type->code][] = $m;
         }
         return $group_machines;
