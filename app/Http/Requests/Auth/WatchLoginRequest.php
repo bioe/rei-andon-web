6657904d410e091fb4f0feb62466919de37c73a9
Eric <erickk89@gmail.com>
2024-09-26 15:36:50 +0800

    form.reset in timer not working properly, refresh the page and store the message in session. Prevent login if the watch login by other people.

--- app/Http/Requests/Auth/WatchLoginRequest.php
+++ app/Http/Requests/Auth/WatchLoginRequest.php
@@ -38,16 +38,27 @@ class WatchLoginRequest extends FormRequest
      */
     public function authenticate(): void
     {
-        $watch = Watch::where('code', $this->watch_code)->where('active', true)->first();
-        $user = User::where('username', $this->username)->where('user_type', OPERATOR)->where('active', true)->first();
+        $watch = Watch::with('login_user')->where('code', $this->watch_code)->where('active', true)->first();
+        $user = User::where('username', $this->username)->where('active', true)->first();
+
+        //Check watch login to any user
+        $watch_by_user_msg = null;
+        if ($watch->login_user != null) {
+            $watch_by_user_msg = $watch->login_user->name . " is login to " . $watch->code;
+        }
+
+        //Check user login to any watch
+        $user_login_watch = null;
+        if ($user) {
+            $user_login_watch = Watch::where('login_user_id', $user->id)->where('active', true)->first();
+        }
 
         $this->watch = $watch;
         $this->user = $user;
-
-        if (!$watch || !$user) {
+        if (!$watch || !$user || $user_login_watch || $watch_by_user_msg) {
             throw ValidationException::withMessages([
-                'watch_code' => $watch == null ? "Watch Code not found." : null,
-                'username' => $user == null ? "Employee Code not found." : null,
+                'watch_code' => $watch == null ? "Watch Code not found." : ($watch_by_user_msg != null ?  $watch_by_user_msg : null),
+                'username' => $user == null ? "Employee Code not found." : ($user_login_watch ? "This employee login to " . $user_login_watch->code : null),
             ]);
         }
     }
