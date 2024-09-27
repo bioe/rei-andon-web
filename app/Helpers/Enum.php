eae73c8b3d42f70f38eeddbbac6b40737f1ab5f7
Eric <erickk89@gmail.com>
2024-09-27 11:33:47 +0800

    update swagger for sendcomplete and separate mqtt topic for REI and WATCH

--- app/Helpers/Enum.php
+++ app/Helpers/Enum.php
@@ -19,12 +19,13 @@ const LATEST_RECORD_VIEW_MINUTE = 15;
 const DASHBOARD_REFRESH_SECOND = 5;
 const JOB_COMPLETE_AFTER_MINUTE = 30; //User will receive new record after the existing record is over x minute and did not update completed_at 
 
-//MQTT
-const TOPIC_NOTIFICATION = "andon/notification";
+//MQTT USE IN REI
 const TOPIC_RESPONSE = "andon/response";
 const TOPIC_COMPLETE = "andon/complete";
-const TOPIC_HEARTBEAT = "andon/heartbeat";
-const TOPIC_LOGIN = "andon/login"; //andon/login/{watch_code}
+//MQTT USE IN WATCH
+const TOPIC_HEARTBEAT = "watch/heartbeat";
+const TOPIC_NOTIFICATION = "watch/notification";
+const TOPIC_LOGIN = "watch/login"; //watch/login/{watch_code}
 
 //WATCH LOGIN MODE
 const WATCH_LOGIN_WEB = "WEB"; //Require to Accept Login in the watch
